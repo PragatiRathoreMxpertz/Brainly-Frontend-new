f4a16202f8c5379c3f703ff1d2c8c0dd58161e34
HatimPresswala <hatimpress786@gmail.com>
2024-04-17 12:44:20 +0530

    added

--- src/helper/axiosconfig.js
+++ src/helper/axiosconfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:3000/api/",
-  baseURL: "https://updated-brainy-lingo-backend.onrender.com/api/",
+  baseURL: "https://final-brainly-backend.onrender.com/api/",
   withCredentials: true,
 });
 
